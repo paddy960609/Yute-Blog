115a83703be87b1e77f51982e46f6723702ce41c
paddy960609 <paddy960609@gmail.com>
2022-12-21 22:52:16 -0600

    UPDATE ENVIRONMENT VARIABLE

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
